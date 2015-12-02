b3911c92c90b11ab1b04b963233d7e3188c1bb89
Luân Vũ <luanhlu3503@gmail.com>
2015-12-02 22:51:40 +0700

    delete unnecessary code

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
 # -*- coding: utf-8 -*- 
-from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import os
-import random
-import string
 from google.appengine.ext.webapp import template
-from google.appengine.ext import blobstore
 
 
 BASE_PATH = os.path.dirname(__file__)
-
 class About(webapp.RequestHandler):
     def get(self, *args):
         path = os.path.join(BASE_PATH, 'templates/index.html')
         self.response.out.write(template.render(path, []))
-class UploadPage(webapp.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        random_key=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30))
-        if user:
-            upload_url = blobstore.create_upload_url('/upload')
-            template_values = {
-            'user':user,
-            'random_key':random_key,
-            'upload_url':upload_url,
-            'nickname': user.nickname(),
-            'login_url': users.create_logout_url("/"),
-            'is_admin': users.is_current_user_admin()
-            }
-            
-            path = os.path.join(BASE_PATH, 'templates/upload.html')
-            self.response.out.write(template.render(path, template_values))
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
 class ViewInfo(webapp.RequestHandler):
     def get(self):
         path =  os.path.join(BASE_PATH, 'templates/view_info.html')
@@ -51,9 +27,7 @@ class MarkTest(webapp.RequestHandler):
             filePath = 'templates/mark_test.html'
         self.response.out.write(template.render(os.path.join(BASE_PATH, filePath), {}))
 application = webapp.WSGIApplication([
-                                    ('/', About),
-                                      # Tạm thời ẩn đi
-#                                       ('/upload-page', UploadPage),
+                                      ('/', About),
                                       ('/ViewInfo', ViewInfo),
                                       ('/structure', Structure),
                                       ('/test', MarkTest),
